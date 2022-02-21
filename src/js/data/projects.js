baad87fe474fd753b8bb4420c698ea31b792825d
RasWeb <rasmuspalm@fastmail.se>
2022-02-21 13:54:07 +0100

    Removed broken code.

--- src/js/data/projects.js
+++
@@ -1,9 +0,0 @@
-// export class Projects {
-//   constructor(title, img, imgAlt, info, link) {
-//     this.title = title;
-//     this.img = img;
-//     this.imgAlt = imgAlt;
-//     this.info = info;
-//     this.linkTo = link;
-//   }
-// }
