6fc4e40fe83c029eba285df448114ade20430c39
RasWeb <rasmuspalm@fastmail.se>
2022-02-21 11:04:32 +0100

    Added some javaScript, dosen't work

---
+++ src/js/data/projects.js
@@ -0,0 +1,9 @@
+export class Projects {
+  constructor(title, img, imgAlt, info, link) {
+    this.title = title;
+    this.img = img;
+    this.imgAlt = imgAlt;
+    this.info = info;
+    this.linkTo = link;
+  }
+}
